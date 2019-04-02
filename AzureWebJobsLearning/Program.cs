a101e06cb2bcd750708c6106fd0467951da40a9a
Devarsh shukla <Devarsh14@gmail.com>
2019-04-02 20:29:36 +1000

    Install necessary packages.

--- AzureWebJobsLearning/Program.cs
+++ AzureWebJobsLearning/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Azure.WebJobs;
+
 
 namespace AzureWebJobsLearning
 {
+    // https://docs.microsoft.com/en-us/azure/app-service/webjobs-sdk-get-started Dot net core uses only 3.0.6 or > version of package.
+    // Packge installed 
+    // 1. Microsoft.Azure.WebJobs
+    // 2. Microsoft.Azure.WebJobs.Extensions
+
+        // Note install both package. otherwise addAzureStorageCoreService will show not available. red line.
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder();
+            builder.ConfigureWebJobs(b =>
+                    {
+                        b.AddAzureStorageCoreServices();
+                    });
+            var host = builder.Build();
+            using (host)
+            {
+                host.Run();
+            }
         }
     }
 }
