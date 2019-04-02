ff935beca2b0471aad8d392aab2d6951123c3cc0
Devarsh shukla <Devarsh14@gmail.com>
2019-04-02 19:33:20 +1000

    Add project files.

---
+++ AzureWebJobsLearning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureWebJobsLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
